dfa411f65dca9d6ceb90338cfc440989bd289b2e
shaoying <shaoying@zuoyebang.com>
2020-09-08 19:52:47 +0800

    opt ssh

---
+++ base/ssh/server_linux.go
@@ -0,0 +1,15 @@
+package ssh
+
+import (
+	"encoding/binary"
+	"syscall"
+	"unsafe"
+)
+
+func _ssh_size(fd uintptr, b []byte) {
+	w := binary.BigEndian.Uint32(b)
+	h := binary.BigEndian.Uint32(b[4:])
+
+	ws := &Winsize{Width: uint16(w), Height: uint16(h)}
+	syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCSWINSZ), uintptr(unsafe.Pointer(ws)))
+}
---
+++ base/ssh/server_windows.go
@@ -0,0 +1,4 @@
+package ssh
+
+func _ssh_size(fd uintptr, b []byte) {
+}

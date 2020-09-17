750e14627e2db84fca5c5c6074f3038dcbe3ea3b
shylinux <shylinux@icloud.com>
2020-09-17 22:20:16 +0800

    opt some

--- core/code/favor.go
+++ core/code/favor.go
@@ -13,7 +13,7 @@ func init() {
 	Index.Merge(&ice.Context{
 		Configs: map[string]*ice.Config{
 			FAVOR: {Name: FAVOR, Help: "收藏夹", Value: kit.Data(
-				kit.MDB_SHORT, "topic", kit.MDB_FIELD, "time,id,type,name,text,file,line",
+				kit.MDB_SHORT, kit.MDB_TOPIC, kit.MDB_FIELD, "time,id,type,name,text,path,file,line",
 			)},
 		},
 		Commands: map[string]*ice.Command{
@@ -22,17 +22,17 @@ func init() {
 					m.Cmdy(mdb.INSERT, m.Prefix(FAVOR), "", mdb.HASH, arg)
 				}},
 				mdb.INSERT: {Name: "insert topic=数据结构 name=hi text=hello", Help: "添加", Hand: func(m *ice.Message, arg ...string) {
-					m.Richs(m.Prefix(FAVOR), "", m.Option("topic"), func(key string, value map[string]interface{}) {
+					m.Richs(m.Prefix(FAVOR), "", m.Option(kit.MDB_TOPIC), func(key string, value map[string]interface{}) {
 						m.Cmdy(mdb.INSERT, m.Prefix(FAVOR), kit.Keys(kit.MDB_HASH, key), mdb.LIST, arg)
 					})
 				}},
 				mdb.MODIFY: {Name: "modify", Help: "编辑", Hand: func(m *ice.Message, arg ...string) {
-					m.Richs(m.Prefix(FAVOR), "", m.Option("topic"), func(key string, value map[string]interface{}) {
+					m.Richs(m.Prefix(FAVOR), "", m.Option(kit.MDB_TOPIC), func(key string, value map[string]interface{}) {
 						m.Cmdy(mdb.MODIFY, m.Prefix(FAVOR), kit.Keys(kit.MDB_HASH, key), mdb.LIST, kit.MDB_ID, m.Option(kit.MDB_ID), arg)
 					})
 				}},
 				mdb.REMOVE: {Name: "remove", Help: "删除", Hand: func(m *ice.Message, arg ...string) {
-					m.Cmdy(mdb.DELETE, m.Prefix(FAVOR), "", mdb.HASH, "topic", m.Option("topic"))
+					m.Cmdy(mdb.DELETE, m.Prefix(FAVOR), "", mdb.HASH, kit.MDB_TOPIC, m.Option(kit.MDB_TOPIC))
 				}},
 				mdb.EXPORT: {Name: "export", Help: "导出", Hand: func(m *ice.Message, arg ...string) {
 					m.Cmdy(mdb.EXPORT, m.Prefix(FAVOR), "", mdb.HASH)
@@ -42,7 +42,7 @@ func init() {
 				}},
 				mdb.INPUTS: {Name: "inputs", Help: "补全", Hand: func(m *ice.Message, arg ...string) {
 					switch arg[0] {
-					case "topic":
+					case kit.MDB_TOPIC:
 						m.Option(mdb.FIELDS, "topic,count,time")
 						m.Cmdy(mdb.SELECT, m.Prefix(FAVOR), "", mdb.HASH)
 					}
@@ -52,7 +52,7 @@ func init() {
 					m.Option("_process", "_field")
 					m.Option("_prefix", "_inner")
 					m.Cmdy(ctx.COMMAND, INNER)
-					m.Append("args", kit.Format([]string{"", m.Option("file"), m.Option("line")}))
+					m.Push("args", kit.Format([]string{m.Option(kit.MDB_PATH), m.Option(kit.MDB_FILE), m.Option(kit.MDB_LINE)}))
 				}},
 			}, Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {
 				if len(arg) > 0 && arg[0] == "_inner" {

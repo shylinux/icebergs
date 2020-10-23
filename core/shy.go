30c0c0505b6245d32e1dc83c4e84101a40c0dc6b
shaoying <shaoying@zuoyebang.com>
2020-10-23 11:27:05 +0800

    opt some

--- core/shy.go
+++
@@ -1,9 +0,0 @@
-package shy
-
-import (
-	_ "github.com/shylinux/icebergs/core/chat"
-	_ "github.com/shylinux/icebergs/core/code"
-	_ "github.com/shylinux/icebergs/core/mall"
-	_ "github.com/shylinux/icebergs/core/team"
-	_ "github.com/shylinux/icebergs/core/wiki"
-)

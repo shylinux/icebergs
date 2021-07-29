f545d36db47f6473f2d02b99a370e1390caa8a18
shylinux <shylinux@icloud.com>
2021-07-29 08:40:22 +0800

    opt icebergs

---
+++ option.go
@@ -0,0 +1,227 @@
+package ice
+
+import (
+	"encoding/json"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	kit "github.com/shylinux/toolkits"
+)
+
+type Sort struct {
+	Fields string
+	Method string
+}
+type Option struct {
+	Name  string
+	Value interface{}
+}
+
+func OptionHash(str string) Option            { return Option{kit.MDB_HASH, str} }
+func OptionFields(str ...string) Option       { return Option{MSG_FIELDS, strings.Join(str, ",")} }
+func (m *Message) OptionFields(str ...string) { m.Option(MSG_FIELDS, strings.Join(str, ",")) }
+func (m *Message) OptionPage(arg ...string) {
+	m.Option("cache.offend", kit.Select("0", arg, 1))
+	m.Option("cache.limit", kit.Select("10", arg, 0))
+}
+func (m *Message) OptionLoad(file string) *Message {
+	if f, e := os.Open(file); e == nil {
+		defer f.Close()
+
+		var data interface{}
+		json.NewDecoder(f).Decode(&data)
+
+		kit.Fetch(data, func(key string, value interface{}) { m.Option(key, kit.Simple(value)) })
+	}
+	return m
+}
+func (m *Message) OptionSplit(fields ...string) (res []string) {
+	for _, k := range strings.Split(strings.Join(fields, ","), ",") {
+		res = append(res, m.Option(k))
+	}
+	return res
+}
+func (m *Message) OptionSimple(key ...string) (res []string) {
+	for _, k := range strings.Split(strings.Join(key, ","), ",") {
+		res = append(res, k, m.Option(k))
+	}
+	return
+}
+func (m *Message) OptionTemplate() string {
+	res := []string{`class="story"`}
+	for _, key := range kit.Split("style") {
+		if m.Option(key) != "" {
+			res = append(res, kit.Format(`s="%s"`, key, m.Option(key)))
+		}
+	}
+	for _, key := range kit.Split("type,name,text") {
+		if m.Option(key) != "" {
+			res = append(res, kit.Format(`data-%s="%s"`, key, m.Option(key)))
+		}
+	}
+	kit.Fetch(m.Optionv("extra"), func(key string, value string) {
+		if value != "" {
+			res = append(res, kit.Format(`data-%s="%s"`, key, value))
+		}
+	})
+	return strings.Join(res, " ")
+}
+
+func (m *Message) Fields(length int, fields ...string) string {
+	return m.Option(MSG_FIELDS, kit.Select(kit.Select("detail", fields, length), m.Option(MSG_FIELDS)))
+}
+func (m *Message) Upload(dir string) {
+	up := kit.Simple(m.Optionv(MSG_UPLOAD))
+	if p := path.Join(dir, up[1]); m.Option(MSG_USERPOD) == "" {
+		// 本机文件
+		m.Cmdy("web.cache", "watch", up[0], p)
+	} else {
+		// 下拉文件
+		m.Cmdy("web.spide", "dev", "save", p, "GET",
+			kit.MergeURL2(m.Option(MSG_USERWEB), path.Join("/share/cache", up[0])))
+	}
+}
+func (m *Message) Action(arg ...string) {
+	m.Option(MSG_ACTION, kit.Format(arg))
+}
+func (m *Message) Status(arg ...interface{}) {
+	args := kit.Simple(arg)
+	list := []map[string]interface{}{}
+	for i := 0; i < len(args)-1; i += 2 {
+		list = append(list, map[string]interface{}{
+			kit.MDB_NAME: args[i], kit.MDB_VALUE: args[i+1],
+		})
+	}
+	m.Option(MSG_STATUS, kit.Format(list))
+}
+func (m *Message) StatusTime(arg ...interface{}) {
+	m.Status(kit.MDB_TIME, m.Time(), arg, "cost", m.FormatCost())
+}
+func (m *Message) StatusTimeCount(arg ...interface{}) {
+	m.Status(kit.MDB_TIME, m.Time(), kit.MDB_COUNT, m.FormatSize(), arg, "cost", m.FormatCost())
+}
+func (m *Message) StatusTimeCountTotal(arg ...interface{}) {
+	m.Status(kit.MDB_TIME, m.Time(), kit.MDB_COUNT, m.FormatSize(), "total", arg, "cost", m.FormatCost())
+}
+
+func (m *Message) Toast(content string, arg ...interface{}) {
+	if len(arg) > 1 {
+		switch val := arg[1].(type) {
+		case string:
+			if value, err := time.ParseDuration(val); err == nil {
+				arg[1] = int(value / time.Millisecond)
+			}
+		}
+	}
+
+	if m.Option(MSG_USERPOD) == "" {
+		m.Cmd("web.space", m.Option(MSG_DAEMON), "toast", "", content, arg)
+	} else {
+		m.Option(MSG_TOAST, kit.Simple(content, arg))
+	}
+}
+func (m *Message) GoToast(title string, cb func(toast func(string, int, int))) {
+	m.Go(func() {
+		cb(func(name string, count, total int) {
+			m.Toast(
+				kit.Format("%s %s/%s", name, strings.TrimSuffix(kit.FmtSize(int64(count)), "B"), strings.TrimSuffix(kit.FmtSize(int64(total)), "B")),
+				kit.Format("%s %d%%", title, count*100/total),
+				kit.Select("1000", "10000", count < total),
+				count*100/total,
+			)
+		})
+	})
+}
+
+func (m *Message) Process(action string, arg ...interface{}) {
+	m.Option(MSG_PROCESS, action)
+	m.Option("_arg", arg...)
+}
+func (m *Message) ProcessRewrite(arg ...interface{}) {
+	m.Process(PROCESS_REWRITE)
+	m.Option("_arg", arg...)
+}
+func (m *Message) ProcessRefresh(delay string) {
+	if d, e := time.ParseDuration(delay); e == nil {
+		m.Option("_delay", int(d/time.Millisecond))
+	}
+	m.Process(PROCESS_REFRESH)
+}
+func (m *Message) ProcessRefresh30ms()  { m.ProcessRefresh("30ms") }
+func (m *Message) ProcessRefresh300ms() { m.ProcessRefresh("300ms") }
+func (m *Message) ProcessRefresh3s()    { m.ProcessRefresh("3s") }
+func (m *Message) ProcessCommand(cmd string, val []string, arg ...string) {
+	if len(arg) > 0 && arg[0] == "run" {
+		m.Cmdy(cmd, arg[1:])
+		return
+	}
+
+	m.Cmdy("command", cmd)
+	m.ProcessField(cmd, "run")
+	m.Push("arg", kit.Format(val))
+}
+func (m *Message) ProcessCommandOpt(arg ...string) {
+	m.Push("opt", kit.Format(m.OptionSimple(arg...)))
+}
+func (m *Message) ProcessField(arg ...interface{}) {
+	m.Process(PROCESS_FIELD)
+	m.Option("_prefix", arg...)
+}
+func (m *Message) ProcessInner() { m.Process(PROCESS_INNER) }
+func (m *Message) ProcessHold()  { m.Process(PROCESS_HOLD) }
+func (m *Message) ProcessBack()  { m.Process(PROCESS_BACK) }
+
+func (m *Message) ShowPlugin(pod, ctx, cmd string, arg ...string) {
+	m.Cmdy("web.space", pod, "context", ctx, "command", cmd)
+	m.Option(MSG_PROCESS, PROCESS_FIELD)
+	m.Option(FIELD_PREFIX, arg)
+}
+func (m *Message) PushPodCmd(cmd string, arg ...string) {
+	m.Table(func(index int, value map[string]string, head []string) {
+		m.Push("pod", m.Option(MSG_USERPOD))
+	})
+
+	m.Cmd("web.space").Table(func(index int, value map[string]string, head []string) {
+		switch value[kit.MDB_TYPE] {
+		case "worker", "server":
+			if value[kit.MDB_NAME] == Info.HostName {
+				break
+			}
+			m.Cmd("web.space", value[kit.MDB_NAME], m.Prefix(cmd), arg).Table(func(index int, val map[string]string, head []string) {
+				val["pod"] = kit.Keys(value[kit.MDB_NAME], val["pod"])
+				m.Push("", val, head)
+			})
+		}
+	})
+}
+func (m *Message) PushSearch(args ...interface{}) {
+	data := kit.Dict(args...)
+	for _, k := range kit.Split(m.Option(MSG_FIELDS)) {
+		switch k {
+		case "pod":
+			// m.Push(k, kit.Select(m.Option(MSG_USERPOD), data[kit.SSH_POD]))
+		case "ctx":
+			m.Push(k, m.Prefix())
+		case "cmd":
+			m.Push(k, kit.Format(data["cmd"]))
+		case kit.MDB_TIME:
+			m.Push(k, kit.Select(m.Time(), data[k]))
+		default:
+			m.Push(k, kit.Format(kit.Select("", data[k])))
+		}
+	}
+}
+func (m *Message) PushSearchWeb(cmd string, name string) {
+	msg := m.Spawn()
+	msg.Option(MSG_FIELDS, "type,name,text")
+	msg.Cmd("mdb.select", m.Prefix(cmd), "", kit.MDB_HASH).Table(func(index int, value map[string]string, head []string) {
+		text := kit.MergeURL(value[kit.MDB_TEXT], value[kit.MDB_NAME], name)
+		if value[kit.MDB_NAME] == "" {
+			text = kit.MergeURL(value[kit.MDB_TEXT] + url.QueryEscape(name))
+		}
+		m.PushSearch("cmd", cmd, kit.MDB_TYPE, kit.Select("", value[kit.MDB_TYPE]), kit.MDB_NAME, name, kit.MDB_TEXT, text)
+	})
+}
---
+++ render.go
@@ -0,0 +1,144 @@
+package ice
+
+import (
+	"strings"
+
+	kit "github.com/shylinux/toolkits"
+)
+
+var renderList = map[string]func(*Message, string, ...interface{}) string{}
+
+func AddRender(key string, render func(*Message, string, ...interface{}) string) {
+	renderList[key] = render
+}
+func Render(m *Message, cmd string, args ...interface{}) string {
+	if render, ok := renderList[cmd]; ok {
+		m.Debug("render: %v %v", cmd, kit.FileLine(render, 3))
+		return render(m, cmd, args...)
+	}
+
+	switch arg := kit.Simple(args...); cmd {
+	case RENDER_ANCHOR: // [name] link
+		return kit.Format(`<a href="%s" target="_blank">%s</a>`, kit.Select(arg[0], arg, 1), arg[0])
+
+	case RENDER_BUTTON: // name...
+		if m._cmd == nil || m._cmd.Meta == nil {
+			break
+		}
+		list := []string{}
+		for _, k := range kit.Split(kit.Join(arg)) {
+			list = append(list, kit.Format(`<input type="button" name="%s" value="%s">`,
+				k, kit.Select(k, kit.Value(m._cmd.Meta, kit.Keys("_trans", k)))))
+		}
+		return kit.Join(list, " ")
+
+	case RENDER_IMAGES: // src [size]
+		return kit.Format(`<img src="%s" height=%s>`, arg[0], kit.Select("120", arg, 1))
+
+	case RENDER_VIDEOS: // src [size]
+		return kit.Format(`<video src="%s" height=%s controls>`, arg[0], kit.Select("120", arg, 1))
+
+	default:
+		return arg[0]
+	}
+	return ""
+}
+func (m *Message) PushRender(key, view, name string, arg ...string) *Message {
+	return m.Push(key, Render(m, view, name, arg))
+}
+func (m *Message) PushDownload(key string, arg ...interface{}) { // [name] file
+	if !m.IsCliUA() {
+		m.Push(key, Render(m, RENDER_DOWNLOAD, arg...))
+	}
+}
+func (m *Message) PushAnchor(arg ...interface{}) { // [name] link
+	if !m.IsCliUA() {
+		m.Push(kit.MDB_LINK, Render(m, RENDER_ANCHOR, arg...))
+	}
+}
+func (m *Message) PushButton(arg ...string) {
+	if !m.IsCliUA() {
+		m.Push(kit.MDB_ACTION, Render(m, RENDER_BUTTON, kit.Join(arg)))
+	}
+}
+func (m *Message) PushScript(arg ...string) *Message { // [type] text...
+	return m.Push(kit.MDB_SCRIPT, Render(m, RENDER_SCRIPT, arg))
+}
+func (m *Message) PushQRCode(key string, text string, arg ...string) { // key text [size]
+	m.Push(key, Render(m, RENDER_QRCODE, text, arg))
+}
+func (m *Message) PushImages(key, src string, arg ...string) { // key src [size]
+	m.Push(key, Render(m, RENDER_IMAGES, src, arg))
+}
+func (m *Message) PushVideos(key, src string, arg ...string) { // key src [size]
+	m.Push(key, Render(m, RENDER_VIDEOS, src, arg))
+}
+func (m *Message) PushAction(list ...interface{}) {
+	m.Table(func(index int, value map[string]string, head []string) {
+		m.PushButton(kit.Simple(list...)...)
+	})
+}
+
+func (m *Message) EchoDownload(arg ...interface{}) { // [name] file
+	m.Echo(Render(m, RENDER_DOWNLOAD, arg...))
+}
+func (m *Message) EchoAnchor(arg ...interface{}) *Message { // [name] link
+	return m.Echo(Render(m, RENDER_ANCHOR, arg...))
+}
+func (m *Message) EchoButton(arg ...string) *Message {
+	return m.Echo(Render(m, RENDER_BUTTON, kit.Join(arg)))
+}
+func (m *Message) EchoScript(arg ...string) *Message {
+	return m.Echo(Render(m, RENDER_SCRIPT, arg))
+}
+func (m *Message) EchoQRCode(text string, arg ...string) *Message { // text [size]
+	return m.Echo(Render(m, RENDER_QRCODE, text, arg))
+}
+func (m *Message) EchoImages(src string, arg ...string) *Message {
+	return m.Echo(Render(m, RENDER_IMAGES, src, arg))
+}
+
+func (m *Message) IsCliUA() bool {
+	if m.Option(MSG_USERUA) == "" || !strings.HasPrefix(m.Option(MSG_USERUA), "Mozilla/5.0") {
+		return true
+	}
+	return false
+}
+func (m *Message) ReplaceLocalhost(url string) string {
+	if strings.Contains(url, "://localhost") {
+		return strings.Replace(url, "localhost", m.Cmd("tcp.host").Append("ip"), 1)
+	}
+	return url
+}
+
+func (m *Message) Render(cmd string, args ...interface{}) *Message {
+	m.Optionv(MSG_OUTPUT, cmd)
+	m.Optionv(MSG_ARGS, args)
+
+	switch cmd {
+	case RENDER_TEMPLATE: // text [data [type]]
+		if len(args) == 1 {
+			args = append(args, m)
+		}
+		if res, err := kit.Render(args[0].(string), args[1]); m.Assert(err) {
+			m.Echo(string(res))
+		}
+	}
+	return m
+}
+func (m *Message) RenderResult(args ...interface{}) *Message {
+	return m.Render(RENDER_RESULT, args...)
+}
+func (m *Message) RenderTemplate(args ...interface{}) *Message {
+
+	return m.Render(RENDER_TEMPLATE, args...)
+}
+func (m *Message) RenderDownload(args ...interface{}) *Message {
+	return m.Render(RENDER_DOWNLOAD, args...)
+}
+func (m *Message) RenderRedirect(args ...interface{}) *Message {
+	return m.Render(RENDER_REDIRECT, args...)
+}
+func (m *Message) RenderIndex(serve, repos string) *Message {
+	return m.RenderDownload(kit.Path(m.Conf(serve, kit.Keym(repos, kit.SSH_PATH)), m.Conf(serve, kit.Keym(repos, kit.SSH_INDEX))))
+}

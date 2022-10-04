801acac1da51585ed873fdc1b068d23c6aeea0de
harveyshao <harveyshao@tencent.com>
2022-10-04 12:22:54 +0800

    opt some

--- base/nfs/grep.go
+++ base/nfs/grep.go
@@ -8,7 +8,7 @@ func init() {
 	Index.MergeCommands(ice.Commands{
 		GREP: {Name: "grep path word auto", Help: "搜索", Hand: func(m *ice.Message, arg ...string) {
 			m.Option("cmd_dir", arg[0])
-			m.Split(m.Cmdx("cli.system", GREP, "--exclude=.[a-z]*", "--exclude-dir=.[a-z]*", "-rni", arg[1]), "file:line:text", ":")
+			m.Split(m.Cmdx("cli.system", GREP, "--exclude=.[a-z]*", "--exclude-dir=.[a-z]*", "-rni", arg[1], "."), "file:line:text", ":")
 		}},
 	})
 }

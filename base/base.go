8772a64efac9e24115f3b99d585063d5dcbb2be4
harveyshao <harveyshao@tencent.com>
2022-12-03 13:40:45 +0800

    opt base

--- base/base.go
+++ base/base.go
@@ -2,17 +2,17 @@ package shy
 
 import (
 	_ "shylinux.com/x/icebergs/base/aaa"
-	_ "shylinux.com/x/icebergs/base/cli"
 	_ "shylinux.com/x/icebergs/base/ctx"
+	_ "shylinux.com/x/icebergs/base/mdb"
 	_ "shylinux.com/x/icebergs/base/web"
 
 	_ "shylinux.com/x/icebergs/base/gdb"
 	_ "shylinux.com/x/icebergs/base/lex"
-	_ "shylinux.com/x/icebergs/base/log"
+	_ "shylinux.com/x/icebergs/base/ssh"
 	_ "shylinux.com/x/icebergs/base/yac"
 
-	_ "shylinux.com/x/icebergs/base/mdb"
+	_ "shylinux.com/x/icebergs/base/cli"
+	_ "shylinux.com/x/icebergs/base/log"
 	_ "shylinux.com/x/icebergs/base/nfs"
-	_ "shylinux.com/x/icebergs/base/ssh"
 	_ "shylinux.com/x/icebergs/base/tcp"
 )

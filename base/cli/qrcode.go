48c9e77cea63905aad1850fb7e63998b3e87f284
harveyshao <harveyshao@tencent.com>
2023-01-12 20:21:29 +0800

    opt inner.css

--- base/cli/qrcode.go
+++ base/cli/qrcode.go
@@ -126,7 +126,7 @@ func init() {
 				}
 			}},
 		}, Hand: func(m *ice.Message, arg ...string) {
-			m.Option(FG, kit.Select(kit.Select(CYAN, BLUE, m.Option(ice.TOPIC) == WHITE), arg, 1))
+			m.Option(FG, kit.Select(kit.Select(BLUE, BLUE, m.Option(ice.TOPIC) == WHITE), arg, 1))
 			m.Option(BG, kit.Select(kit.Select(BLACK, WHITE, m.Option(ice.TOPIC) == WHITE), arg, 2))
 			if m.IsCliUA() {
 				_qrcode_cli(m, kit.Select(kit.Select(ice.Info.Make.Domain, ice.Info.Domain), arg, 0))

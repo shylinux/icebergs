85f3ab850368f59562179f6aee93f7d49bcd70b6
shylinux <shylinux@163.com>
2023-02-26 00:27:49 +0800

    opt some

--- base/web/broad.go
+++ base/web/broad.go
@@ -39,14 +39,14 @@ func _broad_serve(m *ice.Message, host, port string) {
 			}
 			m.Logs(mdb.IMPORT, BROAD, string(buf[:n]), "from", from)
 			msg := m.Spawn(buf[:n])
-			if m.Cmd(BROAD, kit.Format("%s,%s", msg.Option(tcp.HOST), msg.Option(tcp.PORT))).Length() > 0 {
-				continue
-			}
 			if remote := _broad_addr(m, msg.Option(tcp.HOST), msg.Option(tcp.PORT)); remote != nil {
 				m.Cmd(BROAD, func(value ice.Maps) {
 					m.Logs(mdb.EXPORT, BROAD, kit.Format(value), "to", kit.Format(remote))
 					s.WriteToUDP([]byte(m.Spawn(value).FormatMeta()), remote)
 				})
+				if m.Cmd(BROAD, kit.Format("%s,%s", msg.Option(tcp.HOST), msg.Option(tcp.PORT))).Length() > 0 {
+					continue
+				}
 				mdb.HashCreate(m, msg.OptionSimple(tcp.HOST, tcp.PORT))
 			}
 		}

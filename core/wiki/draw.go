914292f072b58cb46bf9a872e0114fde6d88a402
shy <shy@shylinux.com>
2023-10-07 07:48:45 +0800

    opt draw

--- core/wiki/draw.go
+++ core/wiki/draw.go
@@ -7,7 +7,6 @@ import (
 	"shylinux.com/x/icebergs/base/aaa"
 	"shylinux.com/x/icebergs/base/mdb"
 	"shylinux.com/x/icebergs/base/nfs"
-	"shylinux.com/x/icebergs/base/web"
 	kit "shylinux.com/x/toolkits"
 )
 
@@ -15,16 +14,13 @@ const DRAW = "draw"
 
 func init() {
 	Index.MergeCommands(ice.Commands{
-		DRAW: {Name: "draw path=src/main.svg@key pid refresh save actions", Icon: "Grapher.png", Help: "思维导图", Actions: ice.MergeActions(ice.Actions{
+		DRAW: {Name: "draw path=src/main.svg pid list save actions", Icon: "Grapher.png", Help: "思维导图", Actions: ice.MergeActions(ice.Actions{
 			ice.CTX_INIT: {Hand: func(m *ice.Message, arg ...string) {
 				m.Cmd(mdb.RENDER, mdb.CREATE, mdb.TYPE, nfs.SVG, mdb.NAME, m.PrefixKey())
 			}},
 			mdb.RENDER: {Hand: func(m *ice.Message, arg ...string) {
-				defer m.Echo("<html><body>").Echo("</body></html>")
+				defer m.Echo("<!DOCTYPE html><html><body>").Echo("</body></html>")
 				m.Cmdy(nfs.CAT, path.Join(arg[2], arg[1]))
-			}},
-			web.DREAM_TABLES: {Hand: func(m *ice.Message, arg ...string) {
-				kit.Switch(m.Option(mdb.TYPE), kit.Simple(web.SERVER, web.WORKER), func() { m.PushButton(kit.Dict(m.CommandKey(), "导图")) })
 			}},
 		}, aaa.RoleAction(), WikiAction("", nfs.SVG)), Hand: func(m *ice.Message, arg ...string) {
 			kit.If(!_wiki_list(m, arg...), func() {

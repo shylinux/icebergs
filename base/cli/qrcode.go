3fef266c1035e9c7e8d07fe3264f430b4669e87c
shy <shy@shylinux.com>
2023-11-30 14:21:06 +0800

    add some

--- base/cli/qrcode.go
+++ base/cli/qrcode.go
@@ -70,7 +70,8 @@ func init() {
 				m.OptionDefault(FG, BLACK, BG, WHITE)
 				_qrcode_cli(m, kit.Select(kit.Select(ice.Info.Make.Domain, ice.Info.Domain), arg, 0))
 			} else {
-				m.OptionDefault(SIZE, kit.Select("320", "368", m.IsWeixinUA()))
+				m.OptionDefault(SIZE, "320")
+				// m.OptionDefault(SIZE, kit.Select("320", "368", m.IsWeixinUA()))
 				m.Option(FG, kit.Select(m.Option(ice.MSG_FG), arg, 1))
 				m.Option(BG, kit.Select(m.Option(ice.MSG_BG), arg, 2))
 				switch m.Option(ice.MSG_THEME) {

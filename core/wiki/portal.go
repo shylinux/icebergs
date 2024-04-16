4b4179bd04e1ebfdf2e5ef42f40a99d474952679
shy <shy@shylinux.com>
2024-04-16 08:21:24 +0800

    opt some

--- core/wiki/portal.go
+++ core/wiki/portal.go
@@ -71,11 +71,15 @@ const PORTAL = "portal"
 func init() {
 	Index.MergeCommands(ice.Commands{
 		PORTAL: {Name: "portal path auto", Help: "官网", Role: aaa.VOID, Actions: ice.MergeActions(ice.Actions{
+			ice.CTX_INIT: {Hand: func(m *ice.Message, arg ...string) {
+				aaa.White(m, ice.VAR_PORTAL)
+			}},
 			ctx.RUN: {Hand: func(m *ice.Message, arg ...string) {
 				if p := path.Join(ice.VAR_PORTAL, path.Join(arg...)); (m.Option(ice.DEBUG) == ice.TRUE || !nfs.ExistsFile(m, p)) && aaa.Right(m.Spawn(), arg) {
 					ctx.Run(m, arg...)
 					m.Cmd(nfs.SAVE, p, ice.Maps{nfs.CONTENT: m.FormatsMeta(nil), nfs.DIR_ROOT: ""})
 				} else {
+					m.Option(ice.MSG_USERROLE, aaa.TECH)
 					m.Copy(m.Spawn([]byte(m.Cmdx(nfs.CAT, p))))
 				}
 			}},

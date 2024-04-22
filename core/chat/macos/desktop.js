30c457bb07d14679e9e5cd530b75bd812dceef8a
shy <shy@shylinux.com>
2024-04-22 15:27:41 +0800

    add some

--- core/chat/macos/desktop.js
+++ core/chat/macos/desktop.js
@@ -78,7 +78,7 @@ Volcanos(chat.ONIMPORT, {
 		if (can.ConfHeight() > 800) { item.height = can.base.Min(can.ConfHeight()-200, 320, 800), item.top = 50 }
 		if (can.user.isMobile) { item.height = can.ConfHeight()-125, item.top = 25, item.width = can.ConfWidth(), item.left = 0 }
 		item.height = can.base.Max(html.DESKTOP_HEIGHT, item.height), item.width = can.base.Max(html.DESKTOP_WIDTH, item.width)
-		if (can.base.isIn(item.index, web.CODE_VIMER)) { item.width = can.base.Max(1600, can.ConfWidth()), item.left = (can.ConfWidth()-item.width)/2 }
+		// if (can.base.isIn(item.index, web.CODE_VIMER)) { item.width = can.base.Max(1600, can.ConfWidth()), item.left = (can.ConfWidth()-item.width)/2 }
 		item.type = html.PLUGIN, item.style = {left: item.left, top: item.top, height: item.height, width: item.width}
 		can.onappend.plugin(can, item, function(sub) { can.onappend.style(can, html.FLOAT, sub._target), can.ondetail.select(can, sub._target)
 			can.page.style(can, sub._target, html.HEIGHT, item.height, html.WIDTH, item.width)

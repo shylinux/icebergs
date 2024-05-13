b499d09f37a3b1894295d4bd7654231df60c8d80
shy <shy@shylinux.com>
2024-05-14 00:02:33 +0800

    add some

--- core/chat/tutor.js
+++ core/chat/tutor.js
@@ -8,33 +8,39 @@ Volcanos(chat.ONIMPORT, {
 })
 Volcanos(chat.ONACTION, {
 	play: function(can) {
-		can.core.Next(can._msg.Table(), function(value, next, index) {
-			can.onaction.show(can, value.type, value.text), can.onmotion.delay(can, next, 3000)
+		can.core.Next(can._msg.Table(), function(value, next, index, list) { var delay = 3000
+			if (list[index+1]) { delay = (Date.parse(list[index+1].time)-Date.parse(value.time)) }
+			can.onaction.show(can, value.type, value.text, delay), can.onmotion.delay(can, next, delay)
 			can.onmotion.select(can, can.page.SelectOne(can, can._output, "tbody"), html.TR, index)
 		}, function() {
 			can.user.toastSuccess(can, "play done")
 		})
 	},
-	show: function(can, type, text) {
+	show: function(can, type, text, delay) {
 		switch (type) {
+			case "theme":
+				can._root.Header.onimport.theme(can._root.Header, text, {})
+				break
 			case "storm": var ls = text.split(",")
 				can._root.River.onaction.action({}, can._root.River, ls[0], ls[1])
 				break
-			case "theme":
-				can._root.Header.onimport.theme(can._root.Header, text, {})
+			case "click":
+				can.page.Select(can, document.body, text, function(target) {
+					can.core.Next([1, 2, 3, 4, 5], function(value, next, index) { can.page.ClassList.add(can, target, "picker")
+						can.onmotion.delay(can, function() { can.page.ClassList.del(can, target, "picker"), can.onmotion.delay(can, function() { next() }, delay/20) }, delay/20)
+					}, function() {
+						target.click()
+					})
+				})
+				break
+			case "item": var ls = text.split(",")
+				can.page.Select(can, document.body, ls[0], function(target) {
+					can.onmotion.delay(can, function() { target._can.sub.ui[ls[1]].click() })
+				})
 				break
 			case "remove":
 				can.page.Select(can, document.body, text, function(target) {
 					can.page.Remove(can, target)
-				})
-				break
-			case "click":
-				can.page.Select(can, document.body, text, function(target) {
-					target.click()
-					can.page.ClassList.add(can, target, "picker")
-					can.onmotion.delay(can, function() {
-						can.page.ClassList.del(can, target, "picker")
-					}, 3000)
 				})
 				break
 		}

fda6e6f969abbdf5afe02fa1ba21705a27c370ff
shy <shy@shylinux.com>
2024-06-08 08:49:02 +0800

    add some

---
+++ misc/wx/studio.go
@@ -0,0 +1,19 @@
+package wx
+
+import (
+	ice "shylinux.com/x/icebergs"
+	"shylinux.com/x/icebergs/base/ctx"
+	"shylinux.com/x/icebergs/base/mdb"
+	kit "shylinux.com/x/toolkits"
+)
+
+const STUDIO = "studio"
+
+func init() {
+	Index.MergeCommands(ice.Commands{
+		STUDIO: {Hand: func(m *ice.Message, arg ...string) {
+			m.Cmdy(ctx.COMMAND, kit.Split(kit.Select("web.chat.wx.access,web.chat.wx.ide,web.chat.wx.scan", mdb.Config(m, ctx.CMDS))))
+			ctx.DisplayStory(m, "")
+		}},
+	})
+}

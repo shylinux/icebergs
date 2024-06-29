dd974b0980602c50edb4423561029f172a486a14
shy <shy@shylinux.com>
2024-06-29 23:57:15 +0800

    add some

--- core/wiki/feel.go
+++ core/wiki/feel.go
@@ -18,26 +18,24 @@ import (
 const (
 	USR_LOCAL_IMAGE      = "usr/local/image/"
 	USR_IMAGE            = "usr/image/"
+	USR_COVER            = "usr/cover/"
 	USR_AVATAR           = "usr/avatar/"
 	USR_ICONS            = "usr/icons/"
-	SRC_MAIN             = "src/main.ico"
 	USR_ICONS_AVATAR     = "usr/icons/avatar.jpg"
 	USR_ICONS_BACKGROUND = "usr/icons/background.jpg"
-
+	SRC_MAIN             = "src/main.ico"
+)
+const (
 	COVER = "cover"
 )
 const FEEL = "feel"
 
 func init() {
 	Index.MergeCommands(ice.Commands{
-		FEEL: {Name: "feel path=usr/icons/@key file=background.jpg auto upload record1 record2 fullscreen", Help: "影音媒体", Icon: "Photos.png", Role: aaa.VOID, Actions: ice.MergeActions(ice.Actions{
+		FEEL: {Name: "feel path=usr/icons/@key file=background.jpg auto", Help: "影音媒体", Icon: "Photos.png", Role: aaa.VOID, Actions: ice.MergeActions(ice.Actions{
 			mdb.INPUTS: {Hand: func(m *ice.Message, arg ...string) {
-				m.Push(arg[0], USR_LOCAL_IMAGE)
-				m.Push(arg[0], USR_IMAGE)
-				m.Push(arg[0], USR_AVATAR)
-				m.Push(arg[0], USR_ICONS)
+				m.Cmdy("").Cut(nfs.PATH)
 			}},
-			"fullscreen": {Name: "全屏", Icon: "bi bi-fullscreen"},
 			web.UPLOAD: {Hand: func(m *ice.Message, arg ...string) {
 				up := kit.Simple(m.Optionv(ice.MSG_UPLOAD))
 				m.Cmdy(web.CACHE, web.WATCH, m.Option(mdb.HASH), path.Join(m.Option(nfs.PATH), up[1]))
@@ -79,21 +77,35 @@ func init() {
 			}},
 		}, chat.FavorAction(), WikiAction("", "ico|png|PNG|jpg|JPG|jpeg|mp4|m4v|mov|MOV|webm|mp3"), mdb.HashAction(mdb.SHORT, nfs.PATH, mdb.FIELD, "time,path,name,cover")), Hand: func(m *ice.Message, arg ...string) {
 			if len(kit.Slice(arg, 0, 1)) == 0 {
-				mdb.HashSelect(m)
-				if aaa.IsTechOrRoot(m) {
+				if mdb.HashSelect(m); aaa.IsTechOrRoot(m) {
+					m.Push(nfs.PATH, USR_AVATAR).Push(mdb.NAME, "头像库").Push(COVER, USR_ICONS_AVATAR)
 					m.Push(nfs.PATH, USR_LOCAL_IMAGE).Push(mdb.NAME, "私有库").Push(COVER, USR_ICONS_BACKGROUND)
 				}
 				m.Push(nfs.PATH, USR_IMAGE).Push(mdb.NAME, "照片库").Push(COVER, USR_ICONS_BACKGROUND)
-				if aaa.IsTechOrRoot(m) {
-					m.Push(nfs.PATH, USR_AVATAR).Push(mdb.NAME, "头像库").Push(COVER, USR_ICONS_AVATAR)
-				}
+				m.Push(nfs.PATH, USR_COVER).Push(mdb.NAME, "封面库").Push(COVER, USR_ICONS_BACKGROUND)
 				m.Push(nfs.PATH, USR_ICONS).Push(mdb.NAME, "图标库").Push(COVER, SRC_MAIN)
 			} else {
 				if _wiki_list(m, kit.Slice(arg, 0, 1)...); arg[0] == USR_ICONS {
 					m.Sort(mdb.NAME)
 				} else {
-					m.SortStrR(mdb.TIME)
+					switch kit.Select(mdb.TIME, arg, 2) {
+					case mdb.TIME:
+						m.SortStrR(mdb.TIME)
+					case nfs.PATH:
+						m.Sort(nfs.PATH)
+					case nfs.SIZE:
+						m.SortIntR(nfs.SIZE)
+					}
 				}
+				list := m.Spawn().Options(nfs.DIR_DEEP, ice.TRUE).CmdMap(nfs.DIR, USR_COVER+arg[0], nfs.PATH)
+				m.Table(func(value ice.Maps) {
+					p := USR_COVER + kit.TrimSuffix(value[nfs.PATH], ".mp3", ".mp4") + ".jpg"
+					if _, ok := list[p]; ok {
+						m.Push(COVER, p)
+					} else {
+						m.Push(COVER, "")
+					}
+				})
 			}
 			ctx.DisplayLocal(m, "")
 		}},
